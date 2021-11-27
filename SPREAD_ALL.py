c8aadbc64cc2fed3dcd147962261b27e3f7118be
PzzAg6 <57948829+PzzAg6@users.noreply.github.com>
2021-11-27 16:24:11 +0800

    11.27再次

--- SPREAD_ALL.py
+++ SPREAD_ALL.py
@@ -87,14 +87,14 @@ for root, dirs, files in os.walk(os.getcwd()):
 				pkl_file.close()
 		# Max_Node, Max_Layer = Node_Sel_Betw(MULTI_NETWORK, N_LAYERS, N_NODES, SP_Info, RADIUS)
 		Max_Layer = Betw_Layer(MULTI_NETWORK, N_NODES, N_LAYERS, SP_Info)
-		Max_Node = Gravity_Node(MULTI_NETWORK, SP_Info, Max_Layer, N_NODES, RADIUS)#注意带入Max_Layer
+		Max_Node = Gravity_Node(MULTI_NETWORK, SP_Info, Max_Layer, N_LAYERS, N_NODES, RADIUS)#注意带入Max_Layer
 
 		NUM_INFLUENCE, NUM_TIME, TIME_LAYER_LIST = MULTI_NETWORK_SPREAD_SN(MULTI_NETWORK, N_LAYERS, Max_Node, Max_Layer, Wei_Btw_Layer, BETA)
 		DOC_Pre = os.path.split(root)[1]#获取文件名开头
 		Detail_csv(TIME_LAYER_LIST, N_NODES * N_LAYERS, NUM_INFLUENCE, NUM_TIME, BETA,os.path.join(root, DOC_Pre + time.strftime("_%Y_%m_%d_%H_%M_%S", time.localtime())))
 		print('Next...')
 
-
+print("End.")
 
 
 
